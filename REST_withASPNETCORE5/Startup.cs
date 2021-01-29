0ee58b65cf839ebacc56b5a760c64254e0fb3989
gluisjulian <gluisjulian@gmail.com>
2021-01-29 19:32:32 -0300

    start migrations

--- REST_withASPNETCORE5/Startup.cs
+++ REST_withASPNETCORE5/Startup.cs
@@ -7,28 +7,42 @@ using Microsoft.Extensions.Hosting;
 using REST_withASPNETCORE5.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using REST_withASPNETCORE5.Repository.Implementations;
+using Serilog;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace REST_withASPNETCORE5
 {
     public class Startup
     {
-        //private System.Action<DbContextOptionsBuilder> options;
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public IWebHostEnvironment Environment { get; }
 
         public IConfiguration Configuration { get; }
+        //private System.Action<DbContextOptionsBuilder> options;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
 
             var connection = Configuration["MysqlConnection:MysqlConnectionString"];
             services.AddDbContext<MysqlContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
+
 
             //Injeção de Dependencia
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
@@ -53,6 +67,26 @@ namespace REST_withASPNETCORE5
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database Migration Failed", ex);
+
+                throw;
+            }
         }
     }
 }
